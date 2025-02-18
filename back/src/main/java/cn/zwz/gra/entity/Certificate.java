48ec4783f52be07cad0efd3552344e814dbe8962
Prompt工程师 <9815039+laziji000@user.noreply.gitee.com>
2025-02-18 14:42:26 +0800

    首次提交代码

---
+++ back/src/main/java/cn/zwz/gra/entity/Certificate.java
@@ -0,0 +1,42 @@
+package cn.zwz.gra.entity;
+
+import cn.zwz.basics.baseClass.ZwzBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * @author 郑为中
+ * CSDN: Designer 小郑
+ */
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "a_certificate")
+@TableName("a_certificate")
+@ApiModel(value = "证书")
+public class Certificate extends ZwzBaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "证书标题")
+    private String title;
+
+    @ApiModelProperty(value = "学生ID")
+    private String studentId;
+
+    @ApiModelProperty(value = "学生")
+    private String studentName;
+
+    @ApiModelProperty(value = "获得时间")
+    private String date;
+
+    @ApiModelProperty(value = "备注")
+    private String remark;
+}
